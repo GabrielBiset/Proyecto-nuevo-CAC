c4ec056bfbb31c2d31640e88a09c8a0554ced33f
Sebasvit <146362013+Sebasvit@users.noreply.github.com>
2023-12-07 23:36:54 -0300

    Update reserva.js
    corresponde a los cambios hechos en el html

--- TP HTML/scripts/reserva.js
+++ TP HTML/scripts/reserva.js
@@ -8,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
         const comensales = document.getElementById("comensales").value;
         const fecha = document.getElementById("fecha").value;
         const preferencia = document.querySelector('input[name="preferencia"]:checked').value;
+        const nombreReserva = document.getElementById("nombre-reserva").value;
 
         document.getElementById("confirmacion-comensales").textContent = comensales;
         document.getElementById("confirmacion-fecha").textContent = fecha;
         document.getElementById("confirmacion-preferencia").textContent = preferencia;
+        document.getElementById("confirmacion-nombre").textContent = nombreReserva;
 
         confirmacion.style.display = "block";
     });
