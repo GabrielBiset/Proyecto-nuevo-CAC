e8e8fdbff4c176157fdd5b72c2f7bfec35538aab
Sebasvit <146362013+Sebasvit@users.noreply.github.com>
2023-12-16 11:40:50 -0300

    Update formulario.js
    ahora funciona con el backend

--- TP HTML/scripts/formulario.js
+++ TP HTML/scripts/formulario.js
@@ -1,31 +1,40 @@
-function enviarFormulario() {
-    
-    document.getElementById('enviar').disabled = true;
+window.onload = function () {
+    var mySubmitButton = document.getElementById("contactar")
+    var forms = document.getElementsByClassName("needs-validation");
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            if (form.checkValidity() === false) {
 
-    
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-    const telefono = document.getElementById('telefono').value;
-    const descripcion = document.getElementById('descripcion').value;
-
-  
-    if (!validarFormulario(nombre, apellido, email, telefono)) {
-        
-        document.getElementById('enviar').disabled = false;
-        return; 
-    }
-
-    
-    console.log('Enviando datos:', { nombre, apellido, email, telefono, descripcion });
-
-    
-    mostrarMensajeExito("Su mensaje ha sido enviado con éxito.");
-
-    
-    setTimeout(() => {
-        document.getElementById('enviar').disabled = false;
-    }, 2000);
-}
-
+            } else {
+                mySubmitButton.innerHTML = "Enviando..."
+                mySubmitButton.disabled = true;
+                fetch(form.action, { method: 'post', body : new FormData(form), mode : "cors"})
+                .then((response) => {
+                    if (response.status == 200) {
+                        // Agregamos una espera de 3 segundos para dar feedback al usuario..
+                        setTimeout( () => {
+                            mySubmitButton.innerHTML = "Enviar"
+                            mySubmitButton.disabled = false;
+                            form.reset()
+                            form.classList.remove('was-validated'); 
+                            // Recarga la página después de 3 segundos
+                            window.location.reload();     
+                        }, 2000);
+                    } else {
+                        mySubmitButton.innerHTML = "Re-intentar"
+                        mySubmitButton.disabled = false;
+                    }
+                    response.json()
+                })
+                .then(data => console.log(data))
+                .catch(error => console.error('Error al enviar la solicitud:', error));
+            }
+            form.classList.add('was-validated');
+            //alert("Validando... y enviando a backend...")    
+        }, false);
+    });
+       
+};
 
